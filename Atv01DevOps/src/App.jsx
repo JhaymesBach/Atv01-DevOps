c3ce255379fafebefddf91d8b3bbcf9414dca92c
JhaymesBach <jhaymesbachmann@gmail.com>
2026-04-08 15:52:19 -0300

    primeiro-commit

---
+++ Atv01DevOps/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CarCard from "./CarCard"
+export default function App() {
+
+
+  return (
+<div className="container">
+<h1>ZOC AUTOS</h1>
+<CarCard />
+
+</div>
+  )
+}
+
+
---
+++ Atv01DevOps/src/CarCard.jsx
@@ -0,0 +1,10 @@
+import "./App.css"
+
+export default function CarCard() {
+  return (
+    <div className="card">
+      <h2>Gol G3</h2>
+      <p>2005</p>
+    </div>
+  )
+}
