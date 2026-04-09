34d1cd2a8860fe56d48834de6d22b99717cb182f
JhaymesBach <jhaymesbachmann@gmail.com>
2026-04-09 14:50:26 -0300

    feat: adiciona busca de carros por nome

--- Atv01DevOps/src/App.jsx
+++ Atv01DevOps/src/App.jsx
@@ -1,15 +1,29 @@
 import CarCard from "./CarCard"
 import Cars from "./Cars"
 import "./App.css"
+import { useState } from "react"
 
 export default function App() {
+  const [busca, setBusca] = useState("")
   return (
     <div>
       <h1>ZOC AUTOS</h1>
 
-      {Cars.map((car) => (
-        <CarCard key={car.id} car={car} />
-      ))}
+      <input
+        type="text"
+        placeholder="Buscar carro..."
+        value={busca}
+        onChange={(e) => setBusca(e.target.value)}
+      />
+
+      {Cars
+        .filter(car =>
+          car.nome.toLowerCase().includes(busca.toLowerCase())
+        )
+        .map(car => (
+          <CarCard key={car.id} car={car} />
+        ))}
+
     </div>
   )
 }
